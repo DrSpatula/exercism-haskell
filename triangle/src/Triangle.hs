e5723a66c375fcbee9757b49630c5ad0460662bd
Seamus O'Keefe <okeefe77@gmail.com>
2017-03-17 15:15:21 -0400

    Removed Eq typeclass from functions in Triangle solution

--- triangle/src/Triangle.hs
+++ triangle/src/Triangle.hs
@@ -8,7 +8,7 @@ data TriangleType = Equilateral
                   | Illegal
                   deriving (Eq, Show)
 
-inequalityCheck :: (Num a, Eq a, Ord a) => (a,a) -> a -> Bool
+inequalityCheck :: (Num a, Ord a) => (a,a) -> a -> Bool
 inequalityCheck (x,y) z = (x + y) >= z
 
 pairs :: a -> a -> a -> [(a,a)]
@@ -17,22 +17,22 @@ pairs x y z = [(x,y), (y,z), (z,x)]
 allTrue :: [Bool] -> Bool
 allTrue = getAll . foldMap All
 
-passesInequality :: (Num a, Eq a, Ord a) => a -> a -> a -> Bool
+passesInequality :: (Num a, Ord a) => a -> a -> a -> Bool
 passesInequality x y z = allTrue (zipWith inequalityCheck (pairs x y z) [z,x,y])
 
-isIllegal :: (Num a, Eq a, Ord a) => a -> a -> a -> Bool
+isIllegal :: (Num a, Ord a) => a -> a -> a -> Bool
 isIllegal x y z = (not $ passesInequality x y z) || (any ((==) 0) [x,y,z])
 
-isEquilateral :: (Num a, Eq a, Ord a) => a -> a -> a -> Bool
+isEquilateral :: (Num a, Ord a) => a -> a -> a -> Bool
 isEquilateral x y z = allTrue (map (uncurry (==)) (pairs x y z)) 
 
-isScalene :: (Num a, Eq a, Ord a) => a -> a -> a -> Bool
+isScalene :: (Num a, Ord a) => a -> a -> a -> Bool
 isScalene x y z = allTrue (map (uncurry (/=)) (pairs x y z))
 
-isIsosceles :: (Num a, Eq a, Ord a) => a -> a -> a -> Bool
+isIsosceles :: (Num a, Ord a) => a -> a -> a -> Bool
 isIsosceles x y z = (length . filter id) (map (uncurry (==)) (pairs x y z)) == 1
 
-triangleType :: (Num a, Eq a, Ord a) => a -> a -> a -> TriangleType
+triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
 triangleType x y z
   | isIllegal x y z     = Illegal
   | isEquilateral x y z = Equilateral
